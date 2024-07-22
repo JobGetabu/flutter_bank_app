93df28e1e5d89a090761bf9b33a47b8d9ce31008
Job Getabu <getabujob@gmail.com>
2024-07-23 02:17:08 +0400

    ui of the top up page done

--- modules/features/lib/beneficiary/top_up_page.dart
+++ modules/features/lib/beneficiary/top_up_page.dart
@@ -36,75 +36,96 @@ class _TopUpPageState extends State<TopUpPage> {
       body: SafeArea(
         child: Padding(
           padding: EdgeInsets.all(16.r),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'Top up \nBeneficiary',
-                style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 22.sp,
-                    letterSpacing: 1.3,
-                    fontWeight: FontWeight.w500),
-              ),
-              SizedBox(height: 36.h),
-              Text(
-                'Amit Pahandit',
-                style: TextStyle(
-                    color: Palette.accentColor,
-                    fontSize: 14.sp,
-                    fontWeight: FontWeight.w600),
-              ),
-              SizedBox(height: 2.h),
-              Text(
-                '+971526798238',
-                style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 12.sp,
-                    fontWeight: FontWeight.w400),
-              ),
-              SizedBox(height: 4.h),
-              Text(
-                'The maximum you can top up Amit Pahandit this month is AED 2500',
-                style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 12.sp,
-                    fontWeight: FontWeight.w400),
-              ),
-              SizedBox(height: 24.h),
-              Text(
-                'Selected Amount',
-                style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 12.sp,
-                    letterSpacing: 1.3,
-                    fontWeight: FontWeight.w500),
-              ),
-              Text(
-                '$selectedAmount AED',
-                style: TextStyle(
-                    color: Palette.accentColor,
-                    fontSize: 26.sp,
-                    fontWeight: FontWeight.w600),
-              ),
-              SizedBox(height: 24.h),
-              Wrap(
-                spacing: 8.0, // Adjust spacing between items
-                runSpacing: 8.0, // Adjust spacing between rows
-                children: topUpOptions
-                    .map((topUpItem) => TopUpCard(
-                        amount: topUpItem,
-                        onPressed: (topUpItem) {
-                          setState(() {
-                            selectedAmount = topUpItem;
-                          });
-                        }))
-                    .toList(),
-              ),
-            ],
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Top up \nBeneficiary',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 22.sp,
+                      letterSpacing: 1.3,
+                      fontWeight: FontWeight.w500),
+                ),
+                SizedBox(height: 36.h),
+                Text(
+                  'Amit Pahandit',
+                  style: TextStyle(
+                      color: Palette.accentColor,
+                      fontSize: 14.sp,
+                      fontWeight: FontWeight.w600),
+                ),
+                SizedBox(height: 2.h),
+                Text(
+                  '+971526798238',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 12.sp,
+                      fontWeight: FontWeight.w400),
+                ),
+                SizedBox(height: 4.h),
+                Text(
+                  'The maximum you can top up Amit Pahandit this month is AED 2500',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 12.sp,
+                      fontWeight: FontWeight.w400),
+                ),
+                SizedBox(height: 24.h),
+                Text(
+                  'Selected Amount',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 12.sp,
+                      letterSpacing: 1.3,
+                      fontWeight: FontWeight.w500),
+                ),
+                Text(
+                  '$selectedAmount AED',
+                  style: TextStyle(
+                      color: Palette.accentColor,
+                      fontSize: 26.sp,
+                      fontWeight: FontWeight.w600),
+                ),
+                SizedBox(height: 24.h),
+                Wrap(
+                  spacing: 8.0, // Adjust spacing between items
+                  runSpacing: 8.0, // Adjust spacing between rows
+                  children: topUpOptions
+                      .map((topUpItem) => TopUpCard(
+                          amount: topUpItem,
+                          onPressed: (topUpItem) {
+                            setState(() {
+                              selectedAmount = topUpItem;
+                            });
+                          }))
+                      .toList(),
+                ),
+                SizedBox(height: 58.h),
+              ],
+            ),
           ),
         ),
       ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          //Navigator.pushNamed(context, AddBeneficiary.routeName);
+        },
+        label: Text(
+          'Top Up $selectedAmount AED',
+          style: TextStyle(
+              color: Colors.white,
+              fontSize: 16.sp,
+              fontWeight: FontWeight.w400),
+        ),
+
+        backgroundColor: Palette.fadeAccentColor,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
